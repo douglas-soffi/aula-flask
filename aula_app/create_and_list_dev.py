b1c66005debf25316531e939b9b91fe1f8c129df
douglas-soffi <douglassoffi@gmail.com>
2021-08-02 11:02:14 -0300

    tirnando imports não usados

--- aula_app/create_and_list_dev.py
+++ aula_app/create_and_list_dev.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, jsonify
-from flask_restful import Resource, Api
+from flask import request, jsonify
+from flask_restful import Resource
 import json
 from devs import devs
 
